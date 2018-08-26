41b74f18a4c7f11a63ef1db6a4742c405e4787d6
pteczar <p.teczar@gmail.com>
2018-08-26 20:45:01 +0200

    third commit password prompt

--- HelloWorld.c
+++ HelloWorld.c
@@ -4,5 +4,6 @@ int main(int argc, char const *argv[])
     printf("Hello World");
     //this is just to test the GIT 1st commit
     //this is to test the second commit
+    //this is a test of 3rd commit
     return 0;
 }
