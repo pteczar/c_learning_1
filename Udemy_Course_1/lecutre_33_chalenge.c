3a491a84ff226135e66d6b142e91f1ef7dd9a0ae
pteczar <p.teczar@gmail.com>
2018-08-31 18:34:35 +0200

    challenge

---
+++ Udemy_Course_1/lecutre_33_chalenge.c
@@ -0,0 +1,27 @@
+/* Converting number of minutes to days and years
+minutes can be int
+the rest double
+*/
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+
+    double min;
+
+    double years, days;
+    double min_in_a_year = 525948.766;
+   
+    printf("Enter number of minutes:");
+    scanf("%lf",&min);
+
+    days = min/1440;
+
+    printf("days %.11lf\n", days);
+
+
+    years = min / min_in_a_year;
+    printf("years %.11f",years );
+    return 0;
+
+}
+
