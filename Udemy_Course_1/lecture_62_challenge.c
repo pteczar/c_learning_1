094e39db3cca97a804c3dc713aa66d47768d7b21
pteczar <p.teczar@gmail.com>
2018-09-10 20:52:25 +0200

    d

---
+++ Udemy_Course_1/lecture_62_challenge.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/* 
+TIC TAC TOE game
+- 2 players
+- use an array 10 elements ingle dimension (do not use 0)
+- each element of the array represent coordinates on the board
+- display 3x3 grid
+- some function I will need
++ checkForWin - see if the player won or the game is a draw
++ drawBoard - redraws the board for each player turn
++ markBoard - sets the char array with a selection and check for an invalid selection
+
+
+
+
+
+
+
+*/
+
+
+
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+
+
+
+
+    return 0;
+}
