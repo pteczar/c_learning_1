2a3e485ec2e6aa9af383b3d53a016047251ddcd9
pteczar <p.teczar@gmail.com>
2018-09-15 19:24:20 +0200

    book_!

---
+++ Programming_c_book/Functions/1/progam_nr_17.c
@@ -0,0 +1,27 @@
+// calculate factorial recursively
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    unsigned int j;
+    unsigned long int factorial (unsigned int n);
+
+ for( j = 0; j < 11; ++j)
+     printf("%2u! = %lu\n",j, factorial(j));
+ 
+
+    return 0;
+}
+
+// Recursive function to calculate the factorial or positive integer
+
+  unsigned long int factorial (unsigned int n)
+  {
+      unsigned long int result;
+    if ( n == 0)
+        result = 1;
+    else
+        result = n* factorial (n-1);
+
+        return result;
+
+  }
