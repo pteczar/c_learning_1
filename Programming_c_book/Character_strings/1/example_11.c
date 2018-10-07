82a82994c7c5610767fb2f67dce57de2d8c4b112
pteczar <p.teczar@gmail.com>
2018-10-07 21:09:04 +0200

    07.10.2018

--- Programming_c_book/Character_strings/1/example_11.c
+++ Programming_c_book/Character_strings/1/example_11.c
@@ -6,7 +6,7 @@ int strToInt ( const char string[])
 {
     int i, intValue, result = 0;
 
-    for ( i = 0; string[i] >= '0'&& string[i] <= '9'; ++i)
+    for ( i = 0; string[i] >= '0' && string[i] <= '9'; ++i)
     {
         intValue = string[i] - '0';
         result = result * 10 + intValue;
@@ -20,7 +20,7 @@ int main(int argc, char const *argv[])
     int strToInt ( const char string[]);
 
     printf(" %i\n", strToInt("245"));
-     printf(" %i\n", strToInt("100" + 25));
+     printf(" %i\n", strToInt("100") + 25);
       printf(" %i\n", strToInt("13x5"));
     return 0;
 }
