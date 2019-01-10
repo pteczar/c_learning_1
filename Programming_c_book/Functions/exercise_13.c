00055a85b41b8e1da0048c5f447ce8c5d69c0c54
pteczar <p.teczar@gmail.com>
2019-01-10 20:04:38 +0100

    10.01.2019

---
+++ Programming_c_book/Functions/exercise_13.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int sort (int a[], int n, bool c)
+{
+    int i, j, temp;
+
+    if (c == true)
+    {
+
+    for( i = 0; i < n -1; ++i)
+        for(j = i + 1; j < n; ++j)
+            if( a[i] > a[j])
+            {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+    }
+       else if (c != true)
+    {
+        for( i = 0; i < n -1; ++i)
+              for(j = i + 1; j < n; ++j)
+                 if( a[i] < a[j])
+                  {
+                      temp = a[i];
+                      a[i] = a[j];
+                      a[j] = temp;
+                  }
+    }
+    else return 1;
+}
+
+int main(int argc, char const *argv[])
+{
+    int i;
+    int array[16] = {34, -5, 6, 0, 12, 100, 56, 22, 44, -3, -9, 12, 17, 22, 6, 11 };
+
+    int sort (int a[], int n, bool c);
+
+    printf("the array before the sort\n");
+    for( i = 0 ; i < 16; ++i)
+        printf("%i ",array[i]);
+
+        printf("\n");
+
+        sort(array, 16, false);
+
+         printf("the array after the sort\n");
+    for( i = 0 ; i < 16; ++i)
+        printf("%i ",array[i]);
+
+        printf("\n");
+
+    return 0;
+}
