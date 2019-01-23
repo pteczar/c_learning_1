f58514dc7f0e2dfe7cf310a14ea8b3095e99f364
pteczar <p.teczar@gmail.com>
2019-01-23 20:11:32 +0100

    23.01.2019

---
+++ Programming_c_book/Character_strings/program_9.2.c
@@ -0,0 +1,25 @@
+//Function to count the number of characters in a string
+
+#include <stdio.h>
+int stringLength (const char string[]);
+
+int main(int argc, char const *argv[])
+{
+    const char word1[] = "Przemek i dupa";
+    const char word2[] = "is";
+    const char word3[] = "bored";
+
+        printf("%i %i %i", stringLength(word1), stringLength(word2), stringLength(word3));
+
+    return 0;
+}
+
+
+int stringLength (const char string[])
+{
+    int count = 0;
+    while (string[count] != '\0')
+    ++count;
+
+    return count;
+}
