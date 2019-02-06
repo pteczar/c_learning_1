f9c0eaaf243d369f2770f82a42b80abeb19dc948
pteczar <p.teczar@gmail.com>
2019-02-06 23:03:26 +0100

    06.02.2019

---
+++ Programming_c_book/Character_strings/exercise_4.c
@@ -0,0 +1,20 @@
+
+/* two for or while loops one ++ , one -- , printf result or store it in result. return result, i beleive that the function needs to be int */
+
+
+#include <stdio.h>
+char substring (char source, int start, int count, int result);
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
+
+
+
+
+char substring (char source, int start, int count, int result)
+{
+
+}
