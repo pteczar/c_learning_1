33281ba9477aac49b5f073ea889be9d350fe3532
pteczar <p.teczar@gmail.com>
2019-02-07 22:28:44 +0100

    07.02.2019

--- Programming_c_book/Character_strings/exercise_4.c
+++ Programming_c_book/Character_strings/exercise_4.c
@@ -3,18 +3,41 @@
 
 
 #include <stdio.h>
-char substring (char source, int start, int count, int result);
+#include <string.h>
+
+int substring (char source, int start, int count, char result);
 
 int main(int argc, char const *argv[])
 {
-    /* code */
+  char result;
+      char s[100] = "wielki";     
+ 
+    substring (s, 2,4,result);//wtf?
+
+
+
     return 0;
 }
 
 
 
 
-char substring (char source, int start, int count, int result)
+int substring (char source, int start, int count, char result)
 {
+
+int i, j;
+char s[100];
+result = 1;
+
+for (i = 0; i <= start && i != '\0'; i ++)
+{
+    printf("%c",s[i]);
+}
+
+for (j = 0; j <= count && j != '\0'; j--)
+{
+    printf("%c",s[j]);
+}
+
 
 }
