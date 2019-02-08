26c5883616a135ee4472d4b961eb9757935e4066
pteczar <p.teczar@gmail.com>
2019-02-08 22:17:42 +0100

    08.02.2019

--- Programming_c_book/Character_strings/exercise_4.c
+++ Programming_c_book/Character_strings/exercise_4.c
@@ -1,20 +1,20 @@
 
 /* two for or while loops one ++ , one -- , printf result or store it in result. return result, i beleive that the function needs to be int */
 
+// check the 9.6 program 
 
 #include <stdio.h>
 #include <string.h>
 
-int substring (char source, int start, int count, char result);
+int substring (char source[], int start, int count, char result[]);
 
 int main(int argc, char const *argv[])
 {
-  char result;
-      char s[100] = "wielki";     
- 
-    substring (s, 2,4,result);//wtf?
+        char result[100];
+      char s[100] = {"wielki"};     
+     
 
-
+    substring (s, 2,4,result);
 
     return 0;
 }
@@ -22,12 +22,12 @@ int main(int argc, char const *argv[])
 
 
 
-int substring (char source, int start, int count, char result)
+int substring (char source[], int start, int count, char result[])
 {
 
 int i, j;
 char s[100];
-result = 1;
+
 
 for (i = 0; i <= start && i != '\0'; i ++)
 {
