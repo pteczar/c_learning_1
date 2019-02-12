aeb71e68976752a60aa8f5b7fb3c4c148ef5f56a
pteczar <p.teczar@gmail.com>
2019-02-12 22:54:13 +0100

    12.02.2019

--- Programming_c_book/Character_strings/exercise_5.c
+++ Programming_c_book/Character_strings/exercise_5.c
@@ -1,6 +1,8 @@
 /* Write a function called findString() to determine if one character string exists inside another string. The first argument to the function should be the character string that is to be searched and the second argument is the string you are interested in finding. If the function finds the specified string, have it return the location in the source string where the string was found. If the function does not find the string, have it return −1. So, for example, the call Click here to view code image index = findString ("a chatterbox", "hat"); searches the string "a chatterbox" for the string "hat". Because "hat" does exist inside the source string, the function returns 3 to indicate the starting position inside the source string where "hat" was found. */
 
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
 int findString (char motherArgument[], char childArgument[]);
 
@@ -8,28 +10,36 @@ int findString (char motherArgument[], char childArgument[]);
 int main(int argc, char const *argv[])
 {
     
-  //char index = findString ("a chatterbox", "hat"); 
-
-printf("%c",findString ("hat", "h"));
-    return 0;
+    printf("%s",findString ("a chatterbox", "hat")); //why if I remove space ' ' this does not work, why?
+     return 0;
 }
 
 
-int findString (char motherArgument[], char childArgument[]) // is child inside mother? :)
+int findString (char motherArgument[], char childArgument[]) // is child inside the mother? 
 {
  
-    int i = 0;
-    while (motherArgument[i] != '\0')
-    if(motherArgument[i] == childArgument[i])
+    int i, flag = false, freq = 0;
+   
+    for (i = 0; i < strlen(motherArgument); i++) // going through mother
+  {
+    if(motherArgument[i] == childArgument[i]) //  chars equal?
     {
-        i++;
-	
-        return 1;
+        flag = true;
+        printf("The starting position is %d.", i - 1); // i -1 to get 3 as in the example
+        freq++; // frequency
 	}
-	else
-	{
-		return 0;
-	}
-	
+
+  }
+	if (flag = true) 
+      {
+         printf("\nString '%s' occured for %d time(s).\n", childArgument, freq);    
+      }
+
+        else
+            {
+                printf("None\n"); // false = none
+                return -1;
+            }
+    
     
 }
