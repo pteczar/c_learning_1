5c4aab85a10e32f390b360b94c70230771f5da52
pteczar <p.teczar@gmail.com>
2019-02-20 22:29:56 +0100

    20.02.2019

---
+++ Programming_c_book/Character_strings/exercise_11.c
@@ -0,0 +1,29 @@
+/*Extend the strToInt() function from Program 9.11 so that if the first character of the string is a minus sign, the value that follows is taken as a negative number. */
+
+//Function to convert a string to an integer
+    int  strToInt (const char  string[]);
+
+#include <stdio.h>
+
+int strToInt (const char string[])
+{
+    int i, intValue, result = 0;
+
+    for(i = 0; string[i] >= '0' && string[i] <= '9'; ++i)
+    {
+        intValue = string[i] - '0';
+        result = result * 10 + intValue;
+    }
+
+    return result;
+}
+
+int main(int argc, char const *argv[])
+{
+
+        printf ("%i\n", strToInt("245"));
+        printf ("%i\n", strToInt("100") + 25);
+        printf ("%i\n", strToInt("13x5"));    
+
+return 0;
+}
