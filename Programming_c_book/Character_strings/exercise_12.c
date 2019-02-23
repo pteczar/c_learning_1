2300ca1bce85f7e039361e018ac06b701606f411
pteczar <p.teczar@gmail.com>
2019-02-23 22:24:07 +0100

    23.02.2019

--- Programming_c_book/Character_strings/exercise_12.c
+++ Programming_c_book/Character_strings/exercise_12.c
@@ -1,29 +1,50 @@
 /*Write a function called strToFloat() that converts a character string into a floating-point value. Have the function accept an optional leading minus sign. So, the call strToFloat ("-867.6921"); */
 
-//Function to convert a string to an integer
-    int  strToInt (const char  string[]);
+    float strToFloat (const char string[]);
 
 #include <stdio.h>
 
-int strToInt (const char string[])
+
+float strToFloat (const char string[])
 {
-    int i, intValue, result = 0;
+	int i = 0, sign = 1, intValue;
+	float result = 0.0;
 
-    for(i = 0; string[i] >= '0' && string[i] <= '9'; ++i)
-    {
-        intValue = string[i] - '0';
-        result = result * 10 + intValue;
-    }
+	if ( string[0] == '-' ) {
+		++i;
+		sign *= -1;
+	}
 
-    return result;
+	for ( ; string[i] >= '0' && string[i] <= '9'; ++i )
+	{
+		intValue = string[i] - '0';
+		result = result * 10.0 + (float) intValue;
+	}
+
+	if ( string[i] == '.' ) {
+		float placeValue = 1.0;
+
+		for ( int j = 1; string[i + j] >= '0' && string[i + j] <= '9'; ++j ) {
+			placeValue /= 10.0;
+			intValue = string[i + j] - '0';
+			result += placeValue * (float) intValue;
+		}
+	}
+
+	return sign * result;
 }
 
 int main(int argc, char const *argv[])
 {
 
-        printf ("%i\n", strToInt("245"));
-        printf ("%i\n", strToInt("100") + 25);
-        printf ("%i\n", strToInt("13x5"));    
+        printf ("%9.4f\n", strToFloat("-867.6921"));
+        printf ("%9.4f\n", strToFloat("245.1234"));
+	    printf ("%9.4f\n", strToFloat("100.1234") + 25);
+	    printf ("%9.4f\n", strToFloat("13.1234x5"));
+
+	    
+
+	return 0;
 
 return 0;
 }
