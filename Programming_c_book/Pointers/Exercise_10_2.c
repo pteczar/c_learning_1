d411b8d59308ecd3b18f537f2ba0c2375bd402e5
pteczar <p.teczar@gmail.com>
2019-03-30 21:54:22 +0100

    30.03.2019

---
+++ Programming_c_book/Pointers/Exercise_10_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+struct entry
+{
+    int value;
+    struct entry *next;
+};
+
+int main(int argc, char const *argv[])
+{
+
+    struct entry n1, n2;    
+    
+    return 0;
+}
+
+
+
