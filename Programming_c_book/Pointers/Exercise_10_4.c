9d387a5e5d3fe03d2075dbf2017ae4e1f5a9e61f
pteczar <p.teczar@gmail.com>
2019-04-06 20:52:46 +0200

    06.04.2019

---
+++ Programming_c_book/Pointers/Exercise_10_4.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+struct entry
+{
+    int value;
+    struct entry *next;
+};
+
+void printEntry (struct entry head);
+void insertEntry (struct entry *insert, struct entry *previous );
+void removeEntry (struct entry *remove);
+int main(int argc, char const *argv[])
+{
+    struct entry n0, n1, n2, n3, n9;
+
+    
+    struct entry x;
+    x.value = 150;
+    
+    n0.next = &n1;
+    n1.value = 100;
+    n1.next = &n2;
+    n2.value = 200;
+    n2.next = &n3;
+    n3.value = 300;
+    n3.next = NULL;
+    n9.value = 900;
+
+
+        printEntry(n0);
+        insertEntry (&n9, &n0);
+        printEntry(n0);
+        removeEntry(&n0);
+        printEntry(n0);
+
+    return 0;
+}
+
+
+void printEntry (struct entry head)
+{
+    struct entry *ptr = head.next;
+
+    while (ptr != NULL)
+    {
+        printf("%d\n", ptr->value);
+        ptr = ptr->next;
+    }
+
+    printf("\n");
+}
+
+void insertEntry (struct entry *insert, struct entry *previous )
+{
+    insert->next = previous->next;
+    previous->next = insert;
+}
+
+void removeEntry (struct entry *remove)
+{
+   if(remove->next != NULL)
+   {
+       remove->next = remove->next->next;
+   }
+    
+}
---
+++ Programming_c_book/Pointers/Exercise_10_5.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+struct entry
+{
+    int value;
+    struct entry *previous;
+    struct entry *next;
+};
+
+void printDoubleList ( struct entry *ptr);
+int main(int argc, char const *argv[])
+{
+    struct entry  n1, n2, n3;
+    struct entry *nNull = NULL;
+    
+    n1.value = 1;
+    n2.value = 2;
+    n3.value = 3;
+
+    n1.previous = nNull;
+    n2.previous = &n1;
+    n3.previous = &n2;
+   
+    n1.next = &n2;
+    n2.next = &n3;
+    n3.next = nNull;
+
+    
+    printDoubleList(&n1);
+
+    return 0;
+}
+
+
+void printDoubleList ( struct entry *ptr)
+{
+    while (ptr->next != NULL)
+    {
+        printf("%d\n", ptr->value);
+        ptr = ptr->next;
+    }
+
+    while (ptr->previous != NULL)
+    {
+        printf("%d\n", ptr->value);
+        ptr = ptr->previous;
+    }
+        
+    printf ("%i\n", ptr->value);
+}
